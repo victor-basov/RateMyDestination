83cdbbe24d72584506ae29d1159afe1dcdfa9d14
Victor <vbasov10@gmail.com>
2021-07-09 01:46:49 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const pinRoute = require("./routes/pins");
+const userRoute = require("./routes/users");
+
+const port = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+    console.log("MongoDB Connected!")
+}).catch((err)=> console.log(err));
+
+
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
+
+
+app.listen(8800,()=>{
+    console.log("Backend server is running!")
+})
+
+// ... other imports
+const path = require("path")
+
+// ... other app.use middleware
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(5000)
